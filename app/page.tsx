7c75e9464f645579527ac12b41430081fe20dd8a
bello2609 <bellohadi82@gmail.com>
2025-09-15 17:56:33 +0100

    :sparkles: navbar completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
 
-
-export default function Home() {
+const Home =()=> {
   return (
-    <>
+    <div className="px-16">
+      <Navbar />
       <div>This is my home page</div>
-    </>
+    </div>
   )
 }
+export default Home;
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+const Navbar = ()=>{
+    return(
+        <>
+            <div className="flex justify-between items-center">
+                <Image src="/onada.png" alt="onada docs" width={104} height={104} />
+                <nav className="flex justify-evenly font-sans font-medium text-sm text-[#000] w-[500px]">
+                    <Link href="#home">Home</Link>
+                    <Link href="#features">Features</Link>
+                    <Link href="#pricing">Pricing Plans</Link>
+                    <Link href="#testimony">Testimonial</Link>
+                </nav>
+                <Link href="/sign-up" className="flex justify-center items-center font-sans font-bold text-sm text-[#fff] !bg-[#1B1F3B] w-[194px] h-[53px] rounded-[10px]">
+                    Get Started Free
+                </Link>
+            </div>
+        </>
+    );
+}
+export default Navbar;
