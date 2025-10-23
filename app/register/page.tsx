3938441a36606f9253c5763950eeccd9a5f566b6
bello2609 <bellohadi82@gmail.com>
2025-10-23 14:29:49 +0100

    :sparkles: start the development of the register page

---
+++ app/register/page.tsx
@@ -0,0 +1,18 @@
+const Register = ()=>{
+    return(
+        <>
+        <div className="flex w-full h-[100vh]">
+            <div className="flex w-[60%] h-[653px] bg-[url('/Stroke.png')] bg-no-repeat ">
+
+            </div>
+            <div className="flex justify-center  items-center  w-[40%] h-[653px] bg-linear-to-br from-[#0f172a] from-10% via-[#312e1f] via-60% to-[#5a4a20] to-90%">
+                <h3 className="font-sans font-bold w-[300px] text-4xl text-[#fff] border-1 border-red-500">
+                    “Powerful Letters. Effortless Collaboration. Sign up.”
+                </h3>
+            </div>
+        </div>
+        
+        </>
+    );
+}
+export default Register;
